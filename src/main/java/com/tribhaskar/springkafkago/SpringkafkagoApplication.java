872bb769ce2b92eb9451fe99ff72ac24845d3a94
TriBhaskar <panthribhaskar@gmail.com>
2025-05-13 17:08:03 +0530

    first commit

---
+++ src/main/java/com/tribhaskar/springkafkago/SpringkafkagoApplication.java
@@ -0,0 +1,13 @@
+package com.tribhaskar.springkafkago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkafkagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkafkagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tribhaskar/springkafkago/SpringkafkagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tribhaskar.springkafkago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
